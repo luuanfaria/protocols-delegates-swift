81a84a74416915c7eae665b27e46e0cddd96c934
Luan Faria <luuan.fariaf@gmail.com>
2023-09-14 15:54:40 -0300

    update to git

--- Protocols and Delegates/main.swift
+++ Protocols and Delegates/main.swift
@@ -5,7 +5,63 @@
 //  Created by Luan Faria on 14/09/23.
 //
 
-import Foundation
+protocol AdvancedLifeSupport {
+    func performCPR()
+}
 
-print("Hello, World!")
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?
+    
+    func assessSituation() {
+        print("Can you tell me what happened?")
+    }
+    
+    func medicalEmergency() {
+        delegate?.performCPR()
+    }
+}
+
+struct Paramedic: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("The paramedic does chest compressions, 30 per second.")
+    }
+}
+
+class Doctor: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("The doctor does chest compressions, 30 per second.")
+    }
+    
+    func useStethescope() {
+        print("Listening for heart sounds.")
+    }
+}
+
+class Surgeon: Doctor {
+    override func performCPR() {
+        super.performCPR()
+        print("Sings staying alive by the BeeGees")
+    }
+    
+    func useElectricDrill() {
+        print("Whirr...")
+    }
+}
+
+let luan = EmergencyCallHandler()
+let maju = Paramedic(handler: luan)
+//let luna = Surgeon(handler: luan)
+
+luan.assessSituation()
+luan.medicalEmergency()
 
